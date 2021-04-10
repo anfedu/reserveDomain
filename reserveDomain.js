d37d4987f4c819ff06929dc04cc09ec43246a949
anfedu <firdausnurilahmad@gmail.com>
2021-04-10 23:50:18 +0700

    finish

---
+++ reserveDomain.js
@@ -0,0 +1,14 @@
+const array = ["g", "o", "o", "g", "l", "e", ".", "c", "o", ".", "i", "d"];
+
+const reverseDomain = (arr) => {
+  const a = arr.join("");
+
+  var nameSplit = a.split(".").reverse();
+
+  const name = nameSplit.toString();
+
+  const result = name.replace(/,/g, ".");
+  return result;
+};
+
+console.log(reverseDomain(array));
